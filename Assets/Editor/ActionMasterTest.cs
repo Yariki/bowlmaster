6a73a505a4df22595777ff78e1698c25636e6d64
Yaroslav Lisovsky <iyariki.ya@gmail.com>
2017-05-22 21:14:54 +0300

    added actin master, added test project.

---
+++ Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class ActionMasterTest
+{
+
+    ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+
+    [Test]
+    public void T00ONeStrikeReturnsEndTurn()
+    {
+        ActionMaster actionsMaster = new ActionMaster();
+        Assert.AreEqual(endTurn,actionsMaster.Bowl(10));
+    }
+
+}
+
